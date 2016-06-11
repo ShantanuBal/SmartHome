57e28c6da48bc3f984e5ec0aedb79db6e5cad7ae
Bash <bash@amazon.com>
2016-06-11 14:17:13 -0700

    Initial Commit

---
+++ SmartHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartHome
+//
+//  Created by Bal, Shantanu on 6/11/16.
+//  Copyright © 2016 Bal, Shantanu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
